e66cec28820a6ad9555f8b304eb81f8f76412d73
POWER USER <poweruser@Users-iMac-2.local>
2017-04-18 03:26:09 -0400

    update

--- run.py
+++ run.py
@@ -1,12 +1,6 @@
 from app import app, db, manager, sess
 import os
-if __name__ == "__main__":
-    
+if __name__ == "__main__":  
     db.create_all()
-    app.secret_key = 'super secret key'
-    app.config['SESSION_TYPE'] = 'filesystem'
-    sess.init_app(app)
-    app.debug = True;
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
-    # manager.run()
-     
+    app.debug = True
+    app.run()
