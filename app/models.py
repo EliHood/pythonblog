62239f1bd2b06536c2a6f6b137a5490698a9b937
POWER USER <poweruser@Users-iMac-2.local>
2017-04-19 01:48:41 -0400

    update

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String, unique=True)
+    password = db.Column(db.String(20), unique=True)
     # posts = db.relationship('Post', backref='author', lazy='dynamic')
     
     def __init__(self, username, password):
